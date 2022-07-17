921d69790fd8ffc8f3aa5372e6d9b81a437a3106
Adiamith <97206490+Adiamith@users.noreply.github.com>
2022-07-17 17:18:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,18 +54,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['917561852787'] //ur owner number
+global.ownername = "FLASH SER" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "GitHub: FLASH" //ur github or insta name
+global.location = "India, Kerala, kannur" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['917561852787']
+global.ownertag = '917561852787' //ur tag number
+global.botname = '𝛪𝑇𝑆 𝛭𝛯 𝐹𝐿𝛥𝑆𝛨 V4' //ur bot name
+global.linkz = "https://youtube.com/channel/UCX916Hy3QNHslr3DEa12Dhw" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
